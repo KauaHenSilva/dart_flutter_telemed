fc4342a153a08288d63815de771b6997a3450492
KauaHenSilva <kaua.sbc@gmail.com>
2024-06-23 12:56:23 -0300

    chore: add Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:telemed/screens/home_page.dart';
 import 'package:telemed/utils/my_routes.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -16,7 +22,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
       routes: {
-        MyRoutes.home: (ctx) => const HomePage()
+        MyRoutes.home: (ctx) => const HomePage(),
       },
     );
   }
